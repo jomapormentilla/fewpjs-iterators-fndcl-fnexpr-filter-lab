e464dc2e629ea3b266737786d6b8a5c75bf7c6f7
jomapormentilla <jomapormentilla@gmail.com>
2021-02-03 14:10:38 -0500

    commit tree bbfdfb8099d2c1032d77b9da79c9f2faca6585b2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+function findMatching( drivers, string ) {
+    let match = drivers.filter(d => {
+        return d.toUpperCase() === string.toUpperCase()
+    })
+    return match
+}
+
+function fuzzyMatch( drivers, string ) {
+    let match = drivers.filter(d => {
+        if (d.slice(0, string.length) == string) {
+            return d
+        }
+    })
+    return match
+}
+
+function matchName( drivers, string ) {
+    let match = drivers.filter(d => {
+        if (d["name"] == string) {
+            return d
+        }
+    })
+    return match
+}
